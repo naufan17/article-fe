4e09441d6253df3623adc52d8719171a87e5dafe
Muhammad Naufan <muhammadnaufan719@yahoo.com>
2025-05-30 20:53:15 +0700

    feat: add dobunce fetch data

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -7,13 +7,15 @@ import { Footer } from "@/components/footer";
 import { Hero } from "@/components/hero";
 import { Content } from "@/components/content";
 import { ContentSkeleton } from "@/components/content-skeleton";
+import { useDebounce } from "@/hooks/api/use-debounce";
 
 export default function ArticlePage() {
   const [page, setPage] = useState<number>(1);
   const [limit] = useState<number>(9);
   const [category, setCategory] = useState<string>();
   const [title, setTitle] = useState<string | undefined>(undefined);
-  const { data: articles, isLoading } = useArticle(page, limit, title, category);
+  const debounceTitle = useDebounce(title, 500);
+  const { data: articles, isLoading } = useArticle(page, limit, debounceTitle, category);
   const { data: categories } = useCategory(1, 100);
   
   return (
